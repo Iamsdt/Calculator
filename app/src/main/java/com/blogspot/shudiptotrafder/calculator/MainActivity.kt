a973ed618996bd443a855bd782e74347be3202b3
Iamsdt <shudiptotrafder@gmail.com>
2017-07-28 10:47:43 +0600

    Initial commit

---
+++ app/src/main/java/com/blogspot/shudiptotrafder/calculator/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.blogspot.shudiptotrafder.calculator
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun buNumberEvent(view: View){
+
+        val buSelect = view as Button
+        
+    }
+}
